7797c26e887948faa2c4d85c32cb35a8e9b78f3b
kbh <nine4409@sogang.ac.kr>
2021-07-22 20:46:12 +0900

    AVA-speech

---
+++ data_prep/AVA-Speech/AVA_Speech_convert.py
@@ -0,0 +1,36 @@
+from pydub import AudioSegment
+import numpy as np
+import os,glob 
+from tqdm import tqdm
+from multiprocessing import Pool, cpu_count
+
+sr = 16000
+
+input_root =  '/home/data/kbh/AVTR/AVA-Speech/'
+output_root = '/home/data/kbh/AVTR/AVA-Speech-wav/'
+
+target_list = [x for x in glob.glob(os.path.join(input_root,'*.mp3'))]
+
+def convert(idx):
+    target_path = target_list[idx]
+    target_name = target_path.split('/')[-1]
+    target_id = target_name.split('.')[0]
+
+    sound = AudioSegment.from_mp3(target_path)
+    sound = sound.set_frame_rate(sr)
+    sound = sound.set_channels(1)
+
+    # 15 min ~ 30 min
+    sound_arr = sound.get_array_of_samples()
+    sound_arr = sound_arr[15*60*sr:30*60*sr]
+
+    sound = sound._spawn(sound_arr)
+    sound.export(output_root+target_id+'.wav', format="wav")
+
+cpu_num = cpu_count()
+
+os.makedirs(output_root,exist_ok=True)
+
+arr = list(range(len(target_list)))
+with Pool(cpu_num) as p:
+    r = list(tqdm(p.imap(convert, arr), total=len(arr),ascii=True,desc='mp3 to 16kHz mono wav'))
---
+++ data_prep/AVA-Speech/AVA_Speech_download.py
@@ -0,0 +1,90 @@
+from __future__ import unicode_literals
+
+import pandas as pd
+import youtube_dl
+
+import logging
+
+from tqdm import tqdm
+
+prefix='https://youtu.be/'
+
+class MyLogger(object):
+    def __init__(self):
+        super(MyLogger,self)
+        logging.basicConfig(filename='download.log')
+
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        logging.error(msg + ' : ' + id)
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+    elif d['status'] == 'downloading' :
+        if 'total_bytes' in d :
+            pass#print("Downloading : " + str(round( (d['downloaded_bytes'] / d['total_bytes'] )* 100 , 2) ) + "%")
+        else :
+            pass#print("Downloading : But couldn't get total_bytes" )
+
+username = None
+password = None
+
+with open('../../key/youtube_account.txt') as f:
+    username = f.readline().split()[0]
+    password = f.readline().split()[0]
+    print('username : ' + username)
+    print('password : ' + password)
+
+ydl_opts = {
+    'prefer_ffmpeg':True,
+	'format': 'bestaudio/best',
+     #파일이름 default %(title)s-%(id)s.%(ext)s
+     #'outtmpl':'%(title)s.',
+    #'outtmpl':'downloads/%(title)s.%(ext)s',
+    'outtmpl':'/home/data/kbh/AVTR/AVA-Speech/%(title)s.%(ext)s',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        #'preferredcodec': 'wav',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192'
+    }],
+    'quiet':True,
+    'ignoreerrors':True,
+    'logger': MyLogger(),
+    'username':username,
+    'password':password,
+    'age_limit': 40
+    #'progress_hooks': [my_hook],
+}
+
+failed=True
+
+if __name__ == '__main__':
+
+    if not failed :
+        ava = pd.read_csv('ava_speech_labels_v1.csv',names=['id','start','end','label'])
+
+        list_id = ava['id'].drop_duplicates()
+
+        #for id in list_id.head() :
+        for id in tqdm(list_id):
+            # down_load id  as 16kHz Single, WAV
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl.download([prefix + id])
+
+    # run for failed data
+    else :
+        with open('retry.txt') as f :
+            lines = f.readlines()
+
+            for i in tqdm(lines) :
+                id = i.split()[1]
+                with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                    ydl.download([prefix + id])
+
